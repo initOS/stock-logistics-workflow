d61f3a8e9a4620e98fe08cb45c8bc86ed4307db6
Mathias Francke <mathias.francke@initos.com>
2018-12-20 13:21:59 +0100

    21096 updated pre init hook

--- stock_picking_line_sequence/init_hooks.py
+++ stock_picking_line_sequence/init_hooks.py
@@ -3,40 +3,40 @@
 # Author: Mathias Francke <mathias.francke at initos.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import SUPERUSER_ID
-from openerp.api import Environment
-
+from datetime import datetime
 import logging
 
 _logger = logging.getLogger(__name__)
 
-def pre_init_hook(cr, pool):
+
+def pre_init_hook(cr):
     """
     Creates columns for sequence and sequence2 and fills them automatically to
     speed up installation process in populated database.
     """
+    _logger.info('Working on PRE INIT HOOK...')
+
+    # Add columns
     _logger.info('Adding sequence and sequence2 to stock_move...')
+    t_begin = datetime.now()
     cmd = """
-        ALTER TABLE stock_move 
-         ADD COLUMN sequence  integer,
-         ADD COLUMN sequence2 integer;
+        ALTER TABLE stock_move
+        ADD COLUMN IF NOT EXISTS sequence  integer,
+        ADD COLUMN IF NOT EXISTS sequence2 integer;
         """
     cr.execute(cmd)
+    _logger.info('Time needed: %s', (str(datetime.now() - t_begin),))
 
-    _logger.info('Filling sequence of stock_move with initial value (9999)...')
-    cmd = """
-        UPDATE stock_move 
-           SET sequence = 9999;
-        """
-    cr.execute(cmd)
-
-    _logger.info('Filling sequence2 of stock_move with initial value (9999)...')
+    # fill new columns with data
+    _logger.info('Filling sequence and sequence2 of stock_move with initial '
+                 'value (9999)...')
+    t_begin = datetime.now()
     cmd = """
         UPDATE stock_move 
-           SET sequence2 = 9999;
+           SET sequence = 9999, 
+               sequence2 = 9999;
         """
     cr.execute(cmd)
+    _logger.info('Time needed: %s', (str(datetime.now() - t_begin),))
 
-    # env = Environment(cr, SUPERUSER_ID, {})
-    # stock = env['stock.picking'].search([])
-    # stock._reset_sequence()
+    _logger.info('Finished working on PRE INIT HOOK...')
